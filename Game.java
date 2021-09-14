de1efd51752195f14aae46de45c7426b1f463fcc
AlyaVil <89807857+AlyaVil@users.noreply.github.com>
2021-09-14 19:39:42 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -40,4 +40,79 @@ public class Game {
     }
 
 }
+private int mouseX, mouseY; //мышь
+
+    private boolean leftClick = false;
+
+    private Rectangle stopBounds, refreshBounds;
+
+    private boolean gamePaused = false;
+
+    private boolean gameOver = false;
+
+    private Color[] colors = {Color.decode("#ed1c24"), Color.decode("#ff7f27"), Color.decode("#fff200"),
+            Color.decode("#22b14c"), Color.decode("#00a2e8"), Color.decode("#a349a4"), Color.decode("#3f48cc")};
+    private Random random = new Random();
+
+    private Timer = new Timer(300, new ActionListener() {
+
+        @Override
+        public void actionPerformed( ActionEvente) {
+                buttonLapse.stop();
+        }
+    });
+
+    private int score = 0;
+
+    public Board() {
+
+        pause = ImageLoaderImageLoader.loadImage("/pause.png");
+        refresh = ImageLoaderImageLoader.loadImage("/refresh.png");
+
+        mouseX = 0;
+        mouseY = 0;
+
+        stopBounds = new Rectangle(350, 500, pause.getWidth(), pause.getHeight() + pause.getHeight() / 2);
+        refreshBounds = new Rectangle(350, 500 - refresh.getHeight() - , refresh.getWidth(),
+                refresh.getHeight() + refresh.getHeight() / 2);
+
+
+        looper = new Timer(delay, new GameLooper());
+
+
+        shapes[0] = new Shape(new int[][]{
+                {1, 1, 1, 1} // I shape;
+        }, this, colors[0]);
+
+        shapes[1] = new Shape(new int[][]{
+                {1, 1, 1},
+                {0, 1, 0}, // T shape;
+        }, this, colors[1]);
+
+        shapes[2] = new Shape(new int[][]{
+                {1, 1, 1},
+                {1, 0, 0}, // L shape;
+        }, this, colors[2]);
+
+        shapes[3] = new Shape(new int[][]{
+                {1, 1, 1},
+                {0, 0, 1}, // J shape;
+        }, this, colors[3]);
+
+        shapes[4] = new Shape(new int[][]{
+                {0, 1, 1},
+                {1, 1, 0}, // S shape;
+        }, this, colors[4]);
+
+        shapes[5] = new Shape(new int[][]{
+                {1, 1, 0},
+                {0, 1, 1}, // Z shape;
+        }, this, colors[5]);
+
+        shapes[6] = new Shape(new int[][]{
+                {1, 1},
+                {1, 1}, // O shape;
+        }, this, colors[6]);
+
+    }
 }
