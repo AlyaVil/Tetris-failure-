7cc77e0f3ccbc4d79dcaf5671fdf849e50872fa8
AlyaVil <89807857+AlyaVil@users.noreply.github.com>
2021-09-15 22:43:48 +0300

    Update Board.java

--- Board.java
+++ Board.java
@@ -40,5 +40,186 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
     private int delay = 1000 / FPS;
 
 
+    private int mouseX, mouseY; //мышь
+
+    private boolean leftClick = false;
+
+    private Rectangle stopBounds, refreshBounds;
+
+    private boolean gamePaused = false;
+
+    private boolean gameOver = false;
+
+    private Color[] colors = {Color.decode("#ed1c24"), Color.decode("#ff7f27"), Color.decode("#fff200"),
+            Color.decode("#22b14c"), Color.decode("#00a2e8"), Color.decode("#a349a4"), Color.decode("#3f48cc")};
+    private Random random = new Random();
+
+    private Timer = new Timer(300, new ActionListener() {
+
+        @Override
+        public void actionPerformed( ActionEvente) {
+                buttonLapse.stop();
+        }
+    });
+
+    private int score = 0;
+
+    public Board() {
+
+        pause = ImageLoaderImageLoader.loadImage("/pause.png");
+        refresh = ImageLoaderImageLoader.loadImage("/refresh.png");
+
+        mouseX = 0;
+        mouseY = 0;
+
+        stopBounds = new Rectangle(350, 500, pause.getWidth(), pause.getHeight() + pause.getHeight() / 2);
+        refreshBounds = new Rectangle(350, 500 - refresh.getHeight() - , refresh.getWidth(),
+                refresh.getHeight() + refresh.getHeight() / 2);
+
+
+        looper = new Timer(delay, new GameLooper());
+
+
+        shapes[0] = new Shape(new int[][]{
+                {1, 1, 1, 1} // I shape;
+        }, this, colors[0]);
+
+        shapes[1] = new Shape(new int[][]{
+                {1, 1, 1},
+                {0, 1, 0}, // T shape;
+        }, this, colors[1]);
+
+        shapes[2] = new Shape(new int[][]{
+                {1, 1, 1},
+                {1, 0, 0}, // L shape;
+        }, this, colors[2]);
+
+        shapes[3] = new Shape(new int[][]{
+                {1, 1, 1},
+                {0, 0, 1}, // J shape;
+        }, this, colors[3]);
+
+        shapes[4] = new Shape(new int[][]{
+                {0, 1, 1},
+                {1, 1, 0}, // S shape;
+        }, this, colors[4]);
+
+        shapes[5] = new Shape(new int[][]{
+                {1, 1, 0},
+                {0, 1, 1}, // Z shape;
+        }, this, colors[5]);
+
+        shapes[6] = new Shape(new int[][]{
+                {1, 1},
+                {1, 1}, // O shape;
+        }, this, colors[6]);
+
+    }
+ private  voidupdate() {
+        if (stopBounds.contains(mouseX, mouseY) && leftClick && !buttonLapse.isRunning() && !gameOver) {
+ buttonLapse.start();
+ gamePaused = !gamePaused;
+        }
+
+        if (refreshBounds.contains(mouseX, mouseY) && leftClick) {
+ startGame();
+        }
+
+        if (gamePaused || gameOver) {
+            return;
+        }
+ currentShape.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+ g.setColor(Color.BLACK);
+ g.fillRect(0, 0, getWidth(), getHeight());
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+
+                if (board[row][col] != null) {
+ g.setColor(board[row][col]);
+ g.fillRect(col * blockSize, row * blockSize, blockSize, blockSize);
+                }
+
+            }
+        }
+ g.setColor(nextShape.getColor());
+        for (int row = 0; row < nextShape.getCoords().length; row++) {
+            for (int col = 0; col < nextShape.getCoords()[0].length; col++) {
+                if (nextShape.getCoords()[row][col] != 0) {
+                    g.fillRect(col * 30 + 320, row * 30 + 50, Board.blockSize, Board.blockSize);
+                }
+            }
+        }
+ currentShape.render(g);
+
+        if (stopBounds.contains(mouseX, mouseY)) {
+            g.drawImage(pause.getScaledInstance(pause.getWidth() + 3, pause.getHeight() + 3, BufferedImage.SCALE_DEFAULT), stopBounds.x + 3, stopBounds.y + 3, null);
+        } else {
+            g.drawImage(pause, stopBounds.x, stopBounds.y, null);
+        }
+
+        if (refreshBounds.contains(mouseX, mouseY)) {
+            g.drawImage(refresh.getScaledInstance(refresh.getWidth() + 3, refresh.getHeight() + 3,
+                    BufferedImage.SCALE_DEFAULT), refreshBounds.x + 3, refreshBounds.y + 3, null);
+        } else {
+            g.drawImage(refresh, refreshBounds.x, refreshBounds.y, null);
+        }
+
+        if (gamePaused) {
+            String gamePausedString = "GAME PAUSED";
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Georgia", Font.BOLD, 30));
+            g.drawString(gamePausedString, 35, WindowGame.HEIGHT / 2);
+        }
+        if (gameOver) {
+            String gameOverString = "GAME OVER";
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Georgia", Font.BOLD, 30));
+            g.drawString(gameOverString, 50, WindowGame.HEIGHT / 2);
+        }
+        g.setColor(Color.WHITE);
+
+        g.setFont(new Font("Georgia", Font.BOLD, 20));
+
+        g.drawString("SCORE", WindowGame.WIDTH - 125, WindowGame.HEIGHT / 2);
+        g.drawString(score + "", WindowGame.WIDTH - 125, WindowGame.HEIGHT / 2 + 30);
+
+        g.setColor(Color.WHITE);
+
+        for (int i = 0; i <= boardHeight; i++) {
+            g.drawLine(0, i * blockSize, boardWidth * blockSize, i * blockSize);
+        }
+        for (int j = 0; j <= boardWidth; j++) {
+            g.drawLine(j * blockSize, 0, j * blockSize, boardHeight * 30);
+        }
+    }
+
+    public void setNextShape() {
+        int index = random.nextInt(shapes.length);
+        int colorIndex = random.nextInt(colors.length);
+        nextShape = new Shape(shapes[index].getCoords(), this, colors[colorIndex]);
+    }
+
+    public void setCurrentShape() {
+        currentShape = nextShape;
+        setNextShape();
+
+        for (int row = 0; row < currentShape.getCoords().length; row++) {
+            for (int col = 0; col < currentShape.getCoords()[0].length; col++) {
+                if (currentShape.getCoords()[row][col] != 0) {
+                    if (board[currentShape.getY() + row][currentShape.getX() + col] != null) {
+                        gameOver = true;
+                    }
+                }
+            }
+        }
+
+    }
+    
 
 }
