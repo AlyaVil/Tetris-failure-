2dfca67eb4985cb95cdd796d32d6606ddb15090b
AlyaVil <89807857+AlyaVil@users.noreply.github.com>
2021-09-19 14:06:12 +0300

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -109,3 +109,122 @@ public class Shape {
         } else {
             timePassedFromCollision += deltaTime;
         }
+        deltaX = 0;
+    }
+
+    private void collision() {
+        collision = true;
+        timePassedFromCollision = 0;
+    }
+
+    public void render(Graphics g) {
+
+        g.setColor(color);
+        for (int row = 0; row < coords.length; row++) {
+            for (int col = 0; col < coords[0].length; col++) {
+                if (coords[row][col] != 0) {
+                    g.fillRect(col * 30 + x * 30, row * 30 + y * 30, Board.blockSize, Board.blockSize);
+                }
+            }
+        }
+
+
+
+    }
+
+    private void checkLine() {
+        int size = board.getBoard().length - 1;
+
+        for (int i = board.getBoard().length - 1; i > 0; i--) {
+            int count = 0;
+            for (int j = 0; j < board.getBoard()[0].length; j++) {
+                if (board.getBoard()[i][j] != null) {
+                    count++;
+                }
+
+                board.getBoard()[size][j] = board.getBoard()[i][j];
+            }
+            if (count < board.getBoard()[0].length) {
+                size--;
+            }
+        }
+    }
+
+    public void rotateShape() {
+
+        int[][] rotatedShape = null;
+
+        rotatedShape = transposeMatrix(coords);
+
+        rotatedShape = reverseRows(rotatedShape);
+
+        if ((x + rotatedShape[0].length > 10) || (y + rotatedShape.length > 20)) {
+            return;
+        }
+
+        for (int row = 0; row < rotatedShape.length; row++) {
+            for (int col = 0; col < rotatedShape[row].length; col++) {
+                if (rotatedShape[row][col] != 0) {
+                    if (board.getBoard()[y + row][x + col] != null) {
+                        return;
+                    }
+                }
+            }
+        }
+        coords = rotatedShape;
+    }
+
+    private int[][] transposeMatrix(int[][] matrix) {
+        int[][] temp = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                temp[j][i] = matrix[i][j];
+            }
+        }
+        return temp;
+    }
+
+    private int[][] reverseRows(int[][] matrix) {
+
+        int middle = matrix.length / 2;
+
+        for (int i = 0; i < middle; i++) {
+            int[] temp = matrix[i];
+
+            matrix[i] = matrix[matrix.length - i - 1];
+            matrix[matrix.length - i - 1] = temp;
+        }
+
+        return matrix;
+
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setDeltaX(int deltaX) {
+        this.deltaX = deltaX;
+    }
+
+    public void speedUp() {
+        delay = fast;
+    }
+
+    public void speedDown() {
+        delay = normal;
+    }
+
+    public int[][] getCoords() {
+        return coords;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
